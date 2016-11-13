3ba74adeda01fd5b4eebbb0cdc6b4c1edd3acd29
gooin <gooin@outlook.com>
2016-11-13 16:26:25 +0800

    可以定位到自己的位置,切换视图

--- app/src/main/java/xyz/gooin/mymap/MainActivity.java
+++ app/src/main/java/xyz/gooin/mymap/MainActivity.java
@@ -2,23 +2,54 @@ package xyz.gooin.mymap;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+import static com.baidu.location.LocationClientOption.LOC_SENSITIVITY_HIGHT;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private MapView mMapView;
+    private BaiduMap mBaiduMap;
+
+    // 权限请求.
+    private static final int ACCESS_COARSE_LOCATION  = 1;
+
+
+    //定位相关参数
+    private LocationClient mLocationClient = null;
+    private MyLocationListener mLocationListenter;
+    private double mLatitude;
+    private double mLongitude;
+    private boolean isFirstIn = true;
+    private static final int UPDATE_TIME = 5000;
+    private static final int MINI_DISTANCE = 1;
+    private static int LOCATION_COUTNS = 0;
+
+    //private BDLocation mBDLocation;
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +57,89 @@ public class MainActivity extends AppCompatActivity
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
         //注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
-
         setContentView(R.layout.activity_main);
+
+        initMapView();
+        initLocation();
+        initPermission();
+        initToolBar();
+    }
+
+    private void initPermission() {
+
+    }
+
+
+    private void initLocation() {
+        // 定位sdk
+        mLocationClient = new LocationClient(this);
+        mLocationListenter = new MyLocationListener();
+        // 注册监听器
+        mLocationClient.registerLocationListener(mLocationListenter);
+
+        // 设置定位条件
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); //开启GPS
+        option.setCoorType("bd09ll"); //坐标系统为百度坐标
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy); //定位精度为高精度 GPS+网络
+        option.setOpenAutoNotifyMode
+                (UPDATE_TIME, MINI_DISTANCE, LOC_SENSITIVITY_HIGHT); // 最短定位时间间隔, 最短定位距离间隔,定位变化敏感度
+
+        mLocationClient.setLocOption(option);
+
+    }
+
+
+    private void initMapView() {
+
         // 实例化地图
         mMapView = (MapView) findViewById(R.id.map_view);
+        mMapView.showZoomControls(false);
+        mBaiduMap = mMapView.getMap();
+        MapStatusUpdate update = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.animateMapStatus(update);
+    }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    private class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            MyLocationData data = new MyLocationData.Builder()
+                    .accuracy(bdLocation.getRadius())
+                    .latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLatitude())
+                    .build();
+            mBaiduMap.setMyLocationData(data);
+//            MyLocationConfiguration config = new
+//                    MyLocationConfiguration(MyLocationConfiguration.LocationMode.COMPASS);
+            mLatitude = bdLocation.getLatitude();
+            mLongitude = bdLocation.getLongitude();
+
+            if (isFirstIn) {
+                LatLng latLng = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+//                Log.d(TAG, "lat: " + bdLocation.getLatitude() + " lon: " +bdLocation.getLongitude());
+                MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(latLng);
+                mBaiduMap.animateMapStatus(update);
+                isFirstIn = false;
+            }
+
+        }
+    }
+
+    private void initToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "HELLO~", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // 移动到我的位置
+                LatLng latLng = new LatLng(mLatitude, mLongitude);
+                MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(latLng);
+                mBaiduMap.animateMapStatus(update);
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
 
@@ -51,22 +151,43 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Log.d(TAG, "onCreate: end" );
     }
-
-
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
+        Log.d(TAG, "onDestroy: 1");
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mMapView.onResume();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // 开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if(mBaiduMap.isMyLocationEnabled())
+        {mLocationClient.start();}
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // 停止定位
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -99,9 +220,30 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.map_common) {
+            mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+            Log.d(TAG, "onOptionsItemSelected: 普通视图");
             return true;
         }
+        if (id == R.id.map_satellite) {
+            mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+            Log.d(TAG, "onOptionsItemSelected: 卫星视图");
+            return true;
+        }
+        if (id == R.id.map_traffic) {
+            if (mBaiduMap.isTrafficEnabled()) {
+                mBaiduMap.setTrafficEnabled(false);
+                Log.d(TAG, "onOptionsItemSelected: 实时交通关闭");
+                item.setTitle(R.string.map_traffic_on);
+            }else{
+                mBaiduMap.setTrafficEnabled(true);
+                Log.d(TAG, "onOptionsItemSelected: 实时交通开启");
+                item.setTitle(R.string.map_traffic_off);
+            }
+
+            return true;
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
